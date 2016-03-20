0cbb2ba9442a8675e40b57f700eb8752f1c129ff
Hitoshi Togasaki <togasakitogatoga@gmail.com>
2016-03-21 00:57:15 +0900

    Update Solver

--- src/Main.cpp
+++ src/Main.cpp
@@ -1576,7 +1576,7 @@ void nthState(vector<State> &states, int beamWidth){
  * -- 「超高速」のみを使用します。
  * -- 「超高速」を使えるだけの忍力を所持している場合に自動的に使用して、thinkByNinja(id) を1回多く呼び出します。
  */
-void think(int depthLimit, int beamWidth=1000) {
+void think(int depthLimit, int beamWidth=700) {
 
   if (remTime <= 10000){//panic mode
     depthLimit = 2;
